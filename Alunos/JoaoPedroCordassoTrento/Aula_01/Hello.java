31a1320cfe3d72cc1b5a01088b1d99230811e10d
JoaoTrento <trentojoao01@gmail.com>
2025-02-21 20:41:31 -0300

    commit 1

---
+++ Alunos/JoaoPedroCordassoTrento/Aula_01/Hello.java
@@ -0,0 +1,10 @@
+package dale;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
---
+++ Alunos/Rivaldo/vai/Hello.java
@@ -0,0 +1,10 @@
+package vai;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
