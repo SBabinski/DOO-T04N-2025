8a532ce457212fede4d8abbd8402ab12c80ec9d3
Leonardo <gabrielleonardo2512@gmail.com>
2025-03-07 20:58:41 -0300

    Alteração arquivo exibir ola mundo

---
+++ Alunos/Leonardo Gabriel/Aula02/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void main(String[]args){
+
+       System.out.println("Hello World");
+
+    }
+
+    public static void olamundo(){
+
+        System.out.println("Ola mundo");
+
+
+     }
+}
